4ba01c550e36060150992b1ee1294eb1509d3846
Josh Remus <josh@swm-its.com>
2024-12-03 22:58:02 -0500

    Just a check-in

---
+++ asteroid.py
@@ -0,0 +1,6 @@
+from circleshape inherit *
+
+def __init__(self, x, y, radius):
+    super().__init__(x, y, radius)
+
+def draw(self, x, y, radius, width):
--- main.py
+++ main.py
@@ -3,23 +3,49 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
     print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
     #print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    # Add sprite groups and add Player class to groups
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    ticker = pygame.time.Clock()
+    dt = 0.0
+
+    # Initialise player object
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        # Set background to black    
         screen.fill("black")
+
+        # iterate through the updateable group and update each object
+        for each in updatable:
+            each.update(dt)
+        #updatable.update(dt)
+        # iterate through the drawable group and update each object
+        for item in drawable:
+            item.draw(screen)
+        #drawable.draw(screen)
+
+        # reset the display
         pygame.display.flip()
 
 
-
+        ttime = ticker.tick(60)
+        dt = ttime / 1000
 
 if __name__ == "__main__":
     main()
